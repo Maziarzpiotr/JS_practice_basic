ea4bd367f0e9ab7e0f5f9cf69b304a9b3e3d9660
Piotr Maziarz <piotr.maziarz@sage.com>
2024-03-04 16:36:20 +0100

    concatenation

---
+++ Chapter_2/Concatenation.js
@@ -0,0 +1,13 @@
+// lets play with concatenation
+let firstName = 'Piotrek';
+let lastName = 'Maziarz';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+fullName = firstName.concat(' ', lastName);
+console.log(fullName);
+const a = 'teksta';
+const b = "tekstb";
+const c = `tekstc`;
+console.log(a,b,c);
+const expName = `${firstName} ${lastName}`;
+console.log(expName);
